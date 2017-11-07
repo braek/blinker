6d3d23c79b22fc7a6df982f17805d88c1bfb23d9
Kristof Verbraeken <kristof.verbraeken@gmail.com>
2017-11-07 20:25:37 +0100

    More explanation in the code

--- blinker.py
+++ blinker.py
@@ -1,14 +1,23 @@
 from RPi import GPIO
 import time
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(26, GPIO.OUT)
-GPIO.setup(20, GPIO.OUT)
+# LED configuration (BCM numbering)
+led1 = 20
+led2 = 26
 
+# Number of seconds between blinks
+t = 1
+
+# Setup
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(led1, GPIO.OUT)
+GPIO.setup(led2, GPIO.OUT)
+
+# Blinker loop
 while True:
-    GPIO.output(20, GPIO.HIGH)
-    GPIO.output(26, GPIO.LOW)
-    time.sleep(1)
-    GPIO.output(20, GPIO.LOW)
-    GPIO.output(26, GPIO.HIGH)
-    time.sleep(1)
+    GPIO.output(led1, GPIO.HIGH)
+    GPIO.output(led2, GPIO.LOW)
+    time.sleep(t)
+    GPIO.output(led1, GPIO.LOW)
+    GPIO.output(led2, GPIO.HIGH)
+    time.sleep(t)
